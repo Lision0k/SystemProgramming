b855b8864d122456a5cb3bcf54c97f323235a155
Лизокк <ya.lizi.pav@gmail.com>
2025-10-21 16:49:47 +0300

    Update Account.java

--- bankaccount/Account.java
+++ bankaccount/Account.java
@@ -1,11 +1,11 @@
-/*
+/**
 * класс управления балансом
 * */
 public class Account {
     public static int balance = 0;
     private static final int waitBalance = 100;
 
-    /*синхронизированный метод увеличивает баланс
+    /**синхронизированный метод увеличивает баланс
      * @param money - количество денег на которое увеличивается баланс
      * */
     public synchronized void topUpMoney(int money){
@@ -14,7 +14,7 @@ public class Account {
         notify();
     }
 
-    /*синхронизированный метод уменьшает баланс
+    /**синхронизированный метод уменьшает баланс
     * @param money - количество денег на которое уменьшается баланс
     * */
     public synchronized void withdrawMoney(int money){
@@ -24,14 +24,14 @@ public class Account {
         }
     }
 
-    /*синхронизиованный метод
-    * @param return true если balance меньше порогового значения для снятия, иначе false
+    /**синхронизиованный метод
+    * @return true если balance меньше порогового значения для снятия, иначе false
     * */
     public synchronized boolean boolWaiting(){
         return balance < waitBalance;
     }
 
-    /*синхронизированный метод выполняется пока баланс меньше порогового значения для снятия
+    /**синхронизированный метод выполняется пока баланс меньше порогового значения для снятия
     * проверяет условие и ожидает увеличения баланса
     * */
     public synchronized void waiting() throws InterruptedException {
